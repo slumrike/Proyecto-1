ec58dcb7b16ce9b8abd6f01fdb17a3d1fea84d8d
jesus <107016337+slumrike@users.noreply.github.com>
2022-10-27 00:17:22 -0400

    Add files via upload

--- Proyecto.c
+++ Proyecto.c
@@ -27,18 +27,55 @@ Node *add_front(Node *listp,Node *newp){
     return newp;
 }
 
+Node *add_end(Node *listp, Node *newp){
+Node *p;
+if(listp==NULL)
+ return newp;
+for(p=listp;p->next!=NULL;p=p->next);
+p->next = newp;
+return listp;
+}
+
+//matrices 
+
+
+int ObtenerElement(Node *matriz[],int size,int posx,int poxy){
+
+}
+
+void AsingElemnt(int fila,int columna,int element,Node *matriz[]){
+
+}
+
+
+
 void main()
 {   
-    int longitud_x,longitud_y;
+    int filas,nodos,n;
     
     printf("ingresar la capacidad de la matriz= \n");
-    scanf("%i \n",&longitud_x);    //cantidad de nodos
-    scanf("%i \n",&longitud_y);
 
+    scanf("%i\n",&nodos);    //cantidad de nodos
+    scanf("%i",&filas);
 
-    
-    Node *array[longitud_y];
-    for(int i=0; i<3;i++);
-     
-     
+    Node *matriz[filas];
+
+    printf("%p",matriz);
+
+    for(int i=0; i < filas ;i++){  
+     for (int j=0;j<nodos;j++){
+        printf("ingresar el valor de la posicion %i:%i ",i,j);
+        scanf("%i",&n);
+
+        if (j==0){
+            
+            matriz[i] = New_nodo(n);}
+        else {
+       matriz[i] = add_end(matriz[i],New_nodo(n));
+     }
+     }
+    }
+
+    printf("el valor de la posicion 0:1 es %i\n",(matriz[0]->next)->value);
+    return; 
 }
